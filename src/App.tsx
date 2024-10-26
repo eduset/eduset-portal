04a1ee5fd4ee41d49823b6324ec44e52415afbb0
Hongje Choi <enc2586@gmail.com>
2024-10-26 13:35:41 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import "./App.css";
+
+const services = [{ name: "충북과학고등학교", link: "https:cbsh.edu-set.com" }];
+function App() {
+  useEffect(() => {
+    if (services.length === 1) {
+      window.location.href = services[0].link;
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="portal-container">
+        <h1 className="portal-title">학생관리시스템으로 이동</h1>
+        <div className="button-container">
+          {services.map((service, index) => (
+            <a key={index} href={service.link} className="service-button">
+              {service.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
